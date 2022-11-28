fbf471a8c920ed5bcef093342695c52c95ea2128
Fynnian <you@example.com>
2022-11-29 00:09:32 +0100

    added go to button to the room list for each room item

--- src/jsMain/kotlin/components/RoomList.kt
+++ src/jsMain/kotlin/components/RoomList.kt
@@ -1,14 +1,18 @@
 package components
 
+import com.fynnian.application.common.AppPaths
 import com.fynnian.application.common.room.Room
 import csstype.FlexDirection
 import csstype.rem
+import mui.icons.material.Launch
+import mui.material.IconButton
 import mui.material.ListItem
 import mui.material.Typography
 import mui.material.styles.TypographyVariant
 import mui.system.sx
 import react.FC
 import react.Props
+import react.router.useNavigate
 
 external interface RoomListProps : Props {
   var rooms: List<Room>
@@ -33,18 +37,24 @@ external interface RoomListItemProp : Props {
 
 val RoomListItem = FC<RoomListItemProp> { props ->
   val room = props.room
+  val navigate = useNavigate()
+
   ListItem {
     sx {
       flexDirection = FlexDirection.row
       gap = 1.rem
     }
     Typography {
-      variant = TypographyVariant.caption
+      variant = TypographyVariant.subtitle1
       + room.code
     }
     Typography {
       variant = TypographyVariant.body1
       + room.title
+    }
+    IconButton {
+      onClick = { navigate(AppPaths.ROOM.path + "/${room.code}")}
+      Launch { }
     }
   }
 }
