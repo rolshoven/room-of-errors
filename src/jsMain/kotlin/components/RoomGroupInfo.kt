25a394808a5d81be1a7fec04280c18e0d3e2546e
Fynnian <you@example.com>
2023-05-12 21:05:32 +0200

    additional checks and workaround for the fuzzy number input field

--- src/jsMain/kotlin/components/RoomGroupInfo.kt
+++ src/jsMain/kotlin/components/RoomGroupInfo.kt
@@ -8,7 +8,6 @@ import mui.material.styles.TypographyVariant
 import mui.system.sx
 import react.*
 import react.dom.onChange
-import web.html.HTMLInputElement
 import web.html.HTMLTextAreaElement
 import web.html.InputType
 
@@ -19,8 +18,10 @@ external interface RoomGroupInfoProps : Props {
 
 val RoomGroupInfo = FC<RoomGroupInfoProps> { props ->
   val (language) = useContext(LanguageContext)
-  val (groupSize, setGroupSize) = useState<Int?>(null)
+  val (groupSize, setGroupSize) = useState<String?>(null)
   val (groupName, setGroupName) = useState<String?>(null)
+
+  val isInvalidSize: Boolean = groupSize?.let { it.toIntOrNull()?.let { i -> i < 1 } ?: true } ?: false
 
   Card {
     CardHeader {
@@ -41,7 +42,7 @@ val RoomGroupInfo = FC<RoomGroupInfoProps> { props ->
         variant = TypographyVariant.body1
         +props.text!!
       }
-
+      Spacer { size = SpacerPropsSize.SMALL }
       FormGroup {
         TextField {
           id = "groupName"
@@ -60,18 +61,21 @@ val RoomGroupInfo = FC<RoomGroupInfoProps> { props ->
           id = "groupSize"
           name = "groupSize"
           required = true
-          type = InputType.number
+          type = InputType.text
           label = ReactNode(I18n.get(language, I18n.TranslationKey.ROOM_GROUP_INFO_GROUP_SIZE_LABEL))
           placeholder = "2"
           value = groupSize ?: ""
-          onChange = { setGroupSize(it.target.unsafeCast<HTMLInputElement>().value.toIntOrNull()) }
+          error = isInvalidSize
+          onChange = {
+            setGroupSize(it.target.unsafeCast<HTMLTextAreaElement>().value.ifBlank { null })
+          }
         }
       }
       Spacer { size = SpacerPropsSize.SMALL }
       Button {
-        disabled = groupName.isNullOrBlank() || groupSize == null
+        disabled = groupName.isNullOrBlank() || groupSize == null || isInvalidSize
         onClick = {
-          props.onGroupInfoSubmit(groupName!!, groupSize!!)
+          props.onGroupInfoSubmit(groupName!!, groupSize!!.toInt())
         }
         +I18n.get(language, I18n.TranslationKey.ROOM_GROUP_INFO_GROUP_BUTTON)
       }
